245309fcf424d9860233b475c7a7b2b0265cf37f
disgustingdev <disgustingdev@gmail.com>
2022-01-21 21:53:17 +0200

    Update NativeAdView.kt

--- android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
+++ android/src/main/kotlin/com/nover/flutternativeadmob/NativeAdView.kt
@@ -9,7 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.google.android.gms.ads.formats.MediaView
+import com.google.android.gms.ads.nativead.MediaView
 import com.google.android.gms.ads.formats.UnifiedNativeAd
 import com.google.android.gms.ads.nativead.NativeAdView
 
